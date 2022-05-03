3251438b681782ce9cf531b5620acb0d7a06207e
citrus77 <jason.lammers@outlook.com>
2022-05-03 15:53:43 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import './transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  Widget chart() {
+    return Container(
+        child: Card(
+      child: Text('CHART!'),
+    ));
+  }
+
+  Widget transactionList() {
+    return Column(
+        children: transactions
+            .map((tx) => Card(
+                  child: Row(children: <Widget>[
+                    Container(
+                      child: Text(tx.amount.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    )
+                  ]),
+                ))
+            .toList());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Center(
+          child: Column(children: <Widget>[
+            chart(),
+            transactionList(),
+          ]),
+        ));
+  }
+}
